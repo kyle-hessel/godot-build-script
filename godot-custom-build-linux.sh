05350034159410050d8037789faf65296fc0d47f
PollutedMind <pollutedmind@proton.me>
2023-10-08 00:17:54 -0700

    Switched all variables to uppercase to better follow shell scripting convention.

--- godot-custom-build-linux.sh
+++ godot-custom-build-linux.sh
@@ -5,14 +5,14 @@ echo "Welcome to the Cult of the Blue Robot."
 
 # go to the correct folder (back one), remove old folders
 echo "Begin a new engine build, rebuild an existing one in this folder, or build templates? (build/rebuild/templates)"
-read build_type
+read BUILD_TYPE
 
 #######################
 ### TEMPLATES ONLY ####
 #######################
 
-# templates are an entire path on their own as they do not require git clones or module compilation as everything should already be there.
-if [ $build_type == "templates" ]
+# templates are an entire branch of their own as they do not require git clones or module compilation as everything should already be there.
+if [ $BUILD_TYPE == "TEMPLATES" ]
 then
     cd ..
     mv godot-* godot
@@ -20,48 +20,48 @@ then
     # build options
     echo "Below settings should match existing build (I need to learn how to cache this)."
     echo "Choose your Godot version (e.g. 4.1.2-stable, 4.1, master)"
-    read godot_version
+    read GODOT_VERSION
     echo "What platform are you building for? (linuxbsd, windows, server, etc.)"
-    read platform
+    read PLATFORM
     echo "What architecture are you building on? (x86_64, arm64, rv64, wasm32, etc.)"
-    read arch
+    read ARCH
     echo "Are you using custom modules?"
-    read custom
+    read CUSTOM
     echo "How many threads would you like to use for building?"
-    read threadcount
+    read THREADCOUNT
     echo "Choose template type (template_debug/template_release/BOTH)"
-    read template_type
+    read TEMPLATE_TYPE
     echo "Enable .NET (C#) support? (yes/no) NOTE: requires .NET SDK 6/7 to be installed, ~/.dotnet may also need to be in PATH."
-    read b_dotnet
+    read B_DOTNET
     echo "Enable dev_mode for warnings as errors and unit testing? (y/n)"
-    read b_devmode
-    if [ $b_devmode == "n" ]
+    read B_DEVMODE
+    if [ $B_DEVMODE == "n" ]
     then
         echo "Enable production mode for better optimization and portability? (y/n)"
-        read b_optimize
+        read B_OPTIMIZE
     else
-        b_optimize="n"
+        B_OPTIMIZE="n"
     fi
     echo "Use Clang instead of GCC to compile? (yes/no)"
-    read b_clang
+    read B_CLANG
 
     cd godot/
 
     # rename Godot folder appropriately (do this late to avoid errors)
-    if [ $custom == 'y' ]
+    if [ $CUSTOM == 'y' ]
     then
-        mv ../godot ../godot-"$godot_version"-custom
+        mv ../godot ../godot-"$GODOT_VERSION"-CUSTOM
     else
-        mv ../godot ../godot-"$godot_version"
+        mv ../godot ../godot-"$GODOT_VERSION"
     fi
 
     echo "#### Now building native export template(s). ####"
-    if [ $template_type == 'BOTH' ]
+    if [ $TEMPLATE_TYPE == 'BOTH' ]
     then
-        scons platform="$platform" arch="$arch" target=template_debug dev_mode="$b_devmode" production="$b_optimize" module_mono_enabled="$b_dotnet" use_llvm="$b_clang" -j"$threadcount"
-        scons platform="$platform" arch="$arch" target=template_release dev_mode="$b_devmode" production="$b_optimize" module_mono_enabled="$b_dotnet" use_llvm="$b_clang" -j"$threadcount"
+        scons PLATFORM="$PLATFORM" ARCH="$ARCH" target=template_debug dev_mode="$B_DEVMODE" production="$B_OPTIMIZE" module_mono_enabled="$B_DOTNET" use_llvm="$B_CLANG" -j"$THREADCOUNT"
+        scons PLATFORM="$PLATFORM" ARCH="$ARCH" target=template_release dev_mode="$B_DEVMODE" production="$B_OPTIMIZE" module_mono_enabled="$B_DOTNET" use_llvm="$B_CLANG" -j"$THREADCOUNT"
     else
-        scons platform="$platform" arch="$arch" target="$template_type" dev_mode="$b_devmode" production="$b_optimize" module_mono_enabled="$b_dotnet" use_llvm="$b_clang" -j"$threadcount"
+        scons PLATFORM="$PLATFORM" ARCH="$ARCH" target="$TEMPLATE_TYPE" dev_mode="$B_DEVMODE" production="$B_OPTIMIZE" module_mono_enabled="$B_DOTNET" use_llvm="$B_CLANG" -j"$THREADCOUNT"
     fi
 
     cd bin/
@@ -72,86 +72,86 @@ then
 #######################
 
 else
-    if [ $build_type == "build" ]
+    if [ $BUILD_TYPE == "build" ]
     then # if building, pick engine version and modules.
         cd ..
         echo "Deleting any old builds from owning folder."
         sudo rm -r godot-* godotsteam limboai voxel
 
         echo "Choose your Godot version (e.g. 4.1-stable, 4.1, master)"
-        read godot_version
+        read GODOT_VERSION
 
         # community engine modules
         echo "Do you want Godot Steam? (y/n) NOTE: requires Steamworks SDK to be unzipped in an adjacent folder called steam_sdk, and put your steam_appid.txt in there, too!"
-        read b_gd_steam
+        read B_GD_STEAM
 
         echo "Do you want LimboAI? (y/n)"
-        read b_limboai
-        if [ $b_limboai == 'y' ]
+        read B_LIMBOAI
+        if [ $B_LIMBOAI == 'y' ]
         then
             echo "Do you want the latest limboai nightly build? (y/n)"
-            read b_limboai_nightly
+            read B_LIMBOAI_NIGHTLY
         fi
 
         echo "Do you want Godot Voxel? (y/n)"
-        read b_gd_voxel
+        read B_GD_VOXEL
     else # if only rebuilding/creating templates, simply rename existing godot folder.
         echo "Welcome to the Cult of the Blue Robot."
         echo "Choose your Godot version (e.g. 4.1.2-stable, 4.1, master)"
-        read godot_version
+        read GODOT_VERSION
         cd ..
         mv godot-* godot
     fi
 
-    if [ $build_type == "rebuild" ]
+    if [ $BUILD_TYPE == "rebuild" ]
     then
         echo "Below settings should match previous builds (I need to learn how to cache this)."
     fi
 
     # build options
     echo "What platform are you building for? (linuxbsd, windows, server, etc.)"
-    read platform
+    read PLATFORM
     echo "What architecture are you building on? (x86_64, arm64, rv64, wasm32, etc.)"
-    read arch
+    read ARCH
     echo "How many threads would you like to use for building?"
-    read threadcount
+    read THREADCOUNT
     echo "Build export templates as well? (y/n)"
-    read templates
-    if [ $templates == "y" ]
+    read TEMPLATES
+    if [ $TEMPLATES == "y" ]
     then
         echo "Choose template type (template_debug/template_release/BOTH)"
-        read template_type
+        read TEMPLATE_TYPE
     fi
     echo "Enable .NET (C#) support? (yes/no) NOTE: requires .NET SDK 6/7 to be installed, ~/.dotnet may also need to be in PATH."
-    read b_dotnet
+    read B_DOTNET
     echo "Enable dev_mode for warnings as errors and unit testing? (y/n)"
-    read b_devmode
-    if [ $b_devmode == "n" ]
+    read B_DEVMODE
+    if [ $B_DEVMODE == "n" ]
     then
         echo "Enable production mode for better optimization and portability? (y/n)"
-        read b_optimize
+        read B_OPTIMIZE
     else
-        b_optimize="n"
+        B_OPTIMIZE="n"
     fi
     echo "Use Clang instead of GCC to compile? (yes/no)"
-    read b_clang
+    read B_CLANG
     echo "Launch editor upon completion? (y/n)"
-    read b_editor
+    read B_EDITOR
 
-    if [ $build_type == "rebuild" ]
+    if [ $BUILD_TYPE == "rebuild" ]
     then
         echo "#### Now cleaning up generated files from previous build. ####"
-        scons --clean platform="$platform" arch="$arch" dev_mode="$b_devmode" production="$b_optimize" target=editor module_mono_enabled="$b_dotnet" use_llvm="$b_clang" -j"$threadcount"
+        scons --clean PLATFORM="$PLATFORM" ARCH="$ARCH" dev_mode="$B_DEVMODE" production="$B_OPTIMIZE" target=editor module_mono_enabled="$B_DOTNET" use_llvm="$B_CLANG" -j"$THREADCOUNT"
     fi
 
-    if [ $build_type == "build" ]
+    if [ $BUILD_TYPE == "build" ]
     then
         # clone godot
         echo "#### Cloning Godot and modules. ####"
-        git clone https://github.com/godotengine/godot.git -b "$godot_version"
+        git clone https://github.com/godotengine/godot.git -b "$GODOT_VERSION"
 
         # clone modules, copy each into godot modules folder
-        if [ $b_gd_steam == 'y' ] 
+        if [ $B_GD_STEAM == 'y' ] 
         then
             git clone https://github.com/CoaguCo-Industries/GodotSteam.git -b v4.4.1
             mv GodotSteam/ godotsteam/
@@ -162,9 +162,9 @@ else
             cp -rv steam_sdk/sdk/redistributable_bin/ godot/modules/godotsteam/sdk/
         fi
 
-        if [ $b_limboai == 'y' ]
+        if [ $B_LIMBOAI == 'y' ]
         then
-            if [ $b_limboai_nightly == 'y' ]
+            if [ $B_LIMBOAI_NIGHTLY == 'y' ]
             then
                 git clone https://github.com/limbonaut/limboai.git -b master
             else
@@ -173,7 +173,7 @@ else
             cp -rv limboai/ godot/modules/
         fi
 
-        if [ $b_gd_voxel == 'y' ]
+        if [ $B_GD_VOXEL == 'y' ]
         then
             git clone https://github.com/Zylann/godot_voxel.git -b master
             mv godot_voxel/ voxel/
@@ -184,15 +184,15 @@ else
     cd godot/
 
     # rename Godot folder appropriately (do this late to avoid errors)
-    if [ $b_gd_steam == 'y' ] || [ $b_limboai == 'y' ] || [ $b_gd_voxel == 'y' ]
+    if [ $B_GD_STEAM == 'y' ] || [ $B_LIMBOAI == 'y' ] || [ $B_GD_VOXEL == 'y' ]
     then
-        mv ../godot ../godot-"$godot_version"-custom
+        mv ../godot ../godot-"$GODOT_VERSION"-custom
     else
-        mv ../godot ../godot-"$godot_version"
+        mv ../godot ../godot-"$GODOT_VERSION"
     fi
 
     # doing this step before the later .NET steps below as this folder should be removed first, NOT after, contrary to what the docs imply.
-    if [ $b_dotnet == 'yes' ] && [ $build_type == "build" ]
+    if [ $B_DOTNET == 'yes' ] && [ $BUILD_TYPE == "build" ]
     then
         # clear godot C# NuGet package cache (see https://github.com/godotengine/godot/issues/44532 and note on Godot docs for compiling .NET)
         sudo rm -r ~/.local/share/godot/mono/GodotNuGetFallbackFolder
@@ -200,60 +200,60 @@ else
 
     echo "#### Now building Godot. ####"
 
-    scons platform="$platform" arch="$arch" target=editor dev_mode="$b_devmode" production="$b_optimize" module_mono_enabled="$b_dotnet" use_llvm="$b_clang" -j"$threadcount"
+    scons PLATFORM="$PLATFORM" ARCH="$ARCH" target=editor dev_mode="$B_DEVMODE" production="$B_OPTIMIZE" module_mono_enabled="$B_DOTNET" use_llvm="$B_CLANG" -j"$THREADCOUNT"
 
     # final godotsteam setup if enabled
-    if [ $build_type == 'build' ] && [ $b_gd_steam == 'y' ] 
+    if [ $BUILD_TYPE == 'build' ] && [ $B_GD_STEAM == 'y' ] 
     then
         cp -rv ../steam_sdk/steam_appid.txt bin/
 
-        if [ $platform == 'linuxbsd' ]
+        if [ $PLATFORM == 'linuxbsd' ]
         then
             cp -rv ../steam_sdk/sdk/redistributable_bin/linux64/libsteam_api.so bin/
         fi
     fi
 
-    if [ $b_dotnet == 'yes' ]
+    if [ $B_DOTNET == 'yes' ]
     then
         echo "#### Generating .NET glue. ####"
         mkdir ~/.local/share/godot/mono/GodotNuGetFallbackFolder
-        ./bin/godot."$platform".editor."$arch".mono --headless --generate-mono-glue modules/mono/glue
+        ./bin/godot."$PLATFORM".editor."$ARCH".mono --headless --generate-mono-glue modules/mono/glue
         ./modules/mono/build_scripts/build_assemblies.py --godot-output-dir=./bin --push-nupkgs-local ~/MyLocalNugetSource
     fi
 
-    if [ $templates == 'y' ]
+    if [ $TEMPLATES == 'y' ]
     then
         echo "#### Now building native export template(s). ####"
-        if [ $template_type == 'BOTH' ]
+        if [ $TEMPLATE_TYPE == 'BOTH' ]
         then
-            scons platform="$platform" arch="$arch" target=template_debug dev_mode="$b_devmode" production="$b_optimize" module_mono_enabled="$b_dotnet" use_llvm="$b_clang" -j"$threadcount"
-            scons platform="$platform" arch="$arch" target=template_release dev_mode="$b_devmode" production="$b_optimize" module_mono_enabled="$b_dotnet" use_llvm="$b_clang" -j"$threadcount"
+            scons PLATFORM="$PLATFORM" ARCH="$ARCH" target=template_debug dev_mode="$B_DEVMODE" production="$B_OPTIMIZE" module_mono_enabled="$B_DOTNET" use_llvm="$B_CLANG" -j"$THREADCOUNT"
+            scons PLATFORM="$PLATFORM" ARCH="$ARCH" target=template_release dev_mode="$B_DEVMODE" production="$B_OPTIMIZE" module_mono_enabled="$B_DOTNET" use_llvm="$B_CLANG" -j"$THREADCOUNT"
         else
-            scons platform="$platform" arch="$arch" target="$template_type" dev_mode="$b_devmode" production="$b_optimize" module_mono_enabled="$b_dotnet" use_llvm="$b_clang" -j"$threadcount"
+            scons PLATFORM="$PLATFORM" ARCH="$ARCH" target="$TEMPLATE_TYPE" dev_mode="$B_DEVMODE" production="$B_OPTIMIZE" module_mono_enabled="$B_DOTNET" use_llvm="$B_CLANG" -j"$THREADCOUNT"
         fi
     fi
 
     cd bin/
     echo "#### Build complete! Enjoy your cult membership! ####"
 
-    if [ $b_editor == 'y' ]
+    if [ $B_EDITOR == 'y' ]
     then
         echo "#### Lauching Godot editor. Safe travels! ####"
 
-        if [ $b_clang == 'yes' ]
+        if [ $B_CLANG == 'yes' ]
         then
-            if [ $b_dotnet == 'yes' ]
+            if [ $B_DOTNET == 'yes' ]
             then
-                ./godot."$platform".editor."$arch".llvm.mono
+                ./godot."$PLATFORM".editor."$ARCH".llvm.mono
             else
-                ./godot."$platform".editor."$arch".llvm
+                ./godot."$PLATFORM".editor."$ARCH".llvm
             fi
         else
-            if [ $b_dotnet == 'yes' ]
+            if [ $B_DOTNET == 'yes' ]
             then
-                ./godot."$platform".editor."$arch".mono
+                ./godot."$PLATFORM".editor."$ARCH".mono
             else
-                ./godot."$platform".editor."$arch"
+                ./godot."$PLATFORM".editor."$ARCH"
             fi
         fi
     fi
