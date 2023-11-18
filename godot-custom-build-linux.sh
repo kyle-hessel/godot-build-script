310c1565d90d4914458a26c3fb773c2a50bcd372
PollutedMind <pollutedmind@proton.me>
2023-11-17 18:21:11 -0700

    Further improved default selections.

--- godot-custom-build-linux.sh
+++ godot-custom-build-linux.sh
@@ -1,81 +1,151 @@
 #!/usr/bin/env bash
-# Godot Custom Linux Build Script 1.0
+# Godot Custom Linux Build Script v1.2
 # Created by Polluted Mind
+# Note: Only tested on Pop OS 22.04.
 # TODO: optimize old file deletion, option to not include modules without further prompts, option for git pull on rebuild, more testing in different target modes
 
 echo "Welcome to the Cult of the Blue Robot."
 
 # go to the correct folder (back one), remove old folders
-echo "Begin a new engine build, rebuild an existing one in this folder, or build templates? (build/rebuild/templates)"
+echo "Begin a new engine build, rebuild an existing one in this folder, or build templates? (BUILD/rebuild/templates)"
 read BUILD_TYPE
+if [ "$BUILD_TYPE" == "" ]
+then
+    BUILD_TYPE="build"
+fi
 
 #######################
 ### TEMPLATES ONLY ####
 #######################
 
 # templates are an entire branch of their own as they do not require git clones or module compilation as everything should already be there.
-if [ $BUILD_TYPE == "TEMPLATES" ]
+if [ $BUILD_TYPE == "templates" ]
 then
     cd ..
     mv godot-* godot
 
     # build options
     echo "Below settings should match existing build (I need to learn how to cache this)."
-    echo "Choose your Godot version (e.g. 4.1.2-stable, 4.1, master)"
+    echo "Choose your Godot version (e.g. 4.1.3-stable, 4.1, master) or default - latest stable release."
     read GODOT_VERSION
-    echo "What platform are you building for? (linuxbsd, windows, macos, server, etc.)"
+    if [ "$GODOT_VERSION" == "" ]
+    then
+        GODOT_VERSION="4.1.3-stable"
+    fi
+    echo "What platform are you building for? (LINUXBSD, windows, macos, server, etc.)"
     read PLATFORM
+    if [ "$PLATFORM" == "" ]
+    then
+        PLATFORM="linuxbsd"
+    fi
     if [ $PLATFORM == "macos" ] # currently unsupported for cross-compile due to MoltenVK errors.
     then
         echo "NOTE: If osxcross initial setup is struggling, try installing the libbz2-dev package for SDK extraction."
         export OSXCROSS_ROOT="$HOME/osxcross"
     fi
-    echo "What architecture are you building on? (x86_64, arm64, rv64, wasm32, etc.)"
+    echo "What architecture are you building on? (X86_64, arm64, rv64, wasm32, etc.)"
     read ARCH
-    echo "Are you using custom modules?"
+    if [ "$ARCH" == "" ]
+    then
+        ARCH="x86_64"
+    fi
+    echo "Are you using custom modules? (y/N)"
     read CUSTOM
-    echo "How many threads would you like to use for building?"
+    if [ "$CUSTOM" == "" ]
+    then
+        CUSTOM="n"
+    fi
+    echo "How many threads would you like to use for building? (default 16)"
     read THREADCOUNT
-    echo "Do you want PCK encryption support? (y/n)"
+    if [ "$THREADCOUNT" == "" ]
+    then
+        THREADCOUNT="16"
+    fi
+    echo "Do you want PCK encryption support? (y/N)"
     read B_ENCRYPT
+    if [ "$B_ENCRYPT" == "" ]
+    then
+        B_ENCRYPT="n"
+    fi
     if [ $B_ENCRYPT == "y" ]
     then
         export SCRIPT_AES256_ENCRYPTION_KEY="8198992e4523238ab8f520d65b7c54b83347c33a6897855547e3639a594d6a91"
     fi
     echo "Choose template type (template_debug/template_release/BOTH)"
     read TEMPLATE_TYPE
-    echo "Enable .NET (C#) support? (yes/no) NOTE: requires .NET SDK 6/7 to be installed, ~/.dotnet may also need to be in PATH."
+    if [ "$TEMPLATE_TYPE" == "" ]
+    then
+        TEMPLATE_TYPE="both"
+    fi
+    echo "Enable .NET (C#) support? (YES/no) NOTE: requires .NET SDK 6/7 to be installed, ~/.dotnet may also need to be in PATH."
     read B_DOTNET
-    echo "Enable dev_mode for warnings as errors and unit testing? (yes/no) This is useful when doing engine development. NOTE: seems to not play well with some modules."
+    if [ "$B_DOTNET" == "" ]
+    then
+        B_DOTNET="yes"
+    fi
+    echo "Enable dev_mode for warnings as errors and unit testing? (yes/NO) This is useful when doing engine development. NOTE: seems to not play well with some modules."
     read B_DEVMODE
+    if [ "$B_DEVMODE" == "" ]
+    then
+        B_DEVMODE="no"
+    fi
     if [ $B_DEVMODE == "no" ]
     then
         B_DEVBUILD="no"
         B_DEBUGSYMBOLS="no"
-        echo "Enable production mode for better optimization and portability? (yes/no) This is useful for exporting final game builds."
+        echo "Enable production mode for better optimization and portability? (yes/NO) This is useful for exporting final game builds."
         read B_OPTIMIZE
+        if [ "$B_OPTIMIZE" == "" ]
+        then
+            B_OPTIMIZE="no"
+        fi
     else
         B_OPTIMIZE="no"
-        echo "Enable dev_build mode for disabling optimization and enabling debug symbols? (yes/no) Also useful for engine development. NOTE: seems to not play well with some modules."
+        echo "Enable dev_build mode for disabling optimization and enabling debug symbols? (yes/NO) Also useful for engine development. NOTE: seems to not play well with some modules."
         read B_DEVBUILD
+        if [ "$B_DEVBUILD" == "" ]
+        then
+            B_DEVBUILD="no"
+        fi
         if [ $B_DEVBUILD == "yes" ]
         then
-            echo "Additionally enable debugging symbols for debugger and profiler support? (yes/no)"
+            echo "Additionally enable debugging symbols for debugger and profiler support? (YES/no)"
             read B_DEBUGSYMBOLS
+            if [ "$B_DEBUGSYMBOLS" == "" ]
+            then
+                B_DEBUGSYMBOLS="yes"
+            fi
         else
             B_DEBUGSYMBOLS="no"
         fi
     fi
-    echo "Choose an optimization level. This should align well with previous choices. (speed_trace/speed/size/debug/none)"
+    echo "Choose an optimization level. This should align well with previous choices. (SPEED_TRACE/speed/size/debug/none)"
     read OPT_LEVEL
-    echo "Use single or double precision? (single/double)"
+    if [ "$OPT_LEVEL" == "" ]
+    then
+        OPT_LEVEL="speed_trace"
+    fi
+    echo "Use single or double precision? (SINGLE/double)"
     read PRECISION_LEVEL
+    if [ "$PRECISION_LEVEL" == "" ]
+    then
+        PRECISION_LEVEL="single"
+    fi
+
     if [ $B_OPTIMIZE == 'no' ]
     then
-        echo "Use Clang & LLVM instead of GCC to compile? (yes/no)"
+        echo "Use Clang & LLVM instead of GCC to compile? (YES/no)"
         read B_CLANG
-        echo "Use the Mold linker for faster compiles? (yes/no) This may not work on all systems after setup."
+        if [ "$B_CLANG" == "" ]
+        then
+            B_CLANG="yes"
+        fi
+        echo "Use the Mold linker for faster compiles? (yes/NO) This may not work on all systems after setup."
         read B_MOLD
+        if [ "$B_MOLD" == "" ]
+        then
+            B_MOLD="no"
+        fi
     else
         B_CLANG="no"
         B_MOLD="no"
@@ -92,7 +162,7 @@ then
     fi
 
     echo "#### Now building export template(s). ####"
-    if [ $TEMPLATE_TYPE == 'BOTH' ]
+    if [ $TEMPLATE_TYPE == 'both' ]
     then
         pyston-scons platform="$PLATFORM" arch="$ARCH" target=template_debug dev_mode="$B_DEVMODE" dev_build="$B_DEVBUILD" debug_symbols="$B_DEBUGSYMBOLS" production="$B_OPTIMIZE" optimize="$OPT_LEVEL" module_mono_enabled="$B_DOTNET" use_llvm="$B_CLANG" precision="$PRECISION_LEVEL" -j"$THREADCOUNT"
         pyston-scons platform="$PLATFORM" arch="$ARCH" target=template_release dev_mode="$B_DEVMODE" dev_build="$B_DEVBUILD" debug_symbols="$B_DEBUGSYMBOLS" production="$B_OPTIMIZE" optimize="$OPT_LEVEL" module_mono_enabled="$B_DOTNET" use_llvm="$B_CLANG" precision="$PRECISION_LEVEL" -j"$THREADCOUNT"
@@ -114,102 +184,202 @@ else
         echo "Deleting any old builds from owning folder."
         sudo rm -r godot-* godotsteam limboai voxel
 
-        echo "Choose your Godot version (e.g. 4.1-stable, 4.1, master)"
+        echo "Choose your Godot version (e.g. 4.1.3-stable, 4.1, master) or default - latest stable release."
         read GODOT_VERSION
-
-        # community engine modules
-        echo "Do you want Godot Steam? (y/n) NOTE: requires Steamworks SDK to be unzipped in an adjacent folder called steam_sdk, and put your steam_appid.txt in there, too!"
-        echo "Double NOTE: Don't cross-compile on Linux for Windows using MinGW with this module; compile natively on Windows using MSVC instead. See GodotSteam Github note."
-        read B_GD_STEAM
-
-        echo "Do you want LimboAI? (y/n)"
-        read B_LIMBOAI
-        if [ $B_LIMBOAI == 'y' ]
+        if [ "$GODOT_VERSION" == "" ]
         then
-            echo "Do you want the latest limboai nightly build? (y/n)"
-            read B_LIMBOAI_NIGHTLY
+            GODOT_VERSION="4.1.3-stable"
+        fi
+        
+        echo "Include community modules? (y/N)"
+        read B_COMMUNITY
+        if [ "$B_COMMUNITY" = "" ]
+        then
+            B_COMMUNITY="n"
         fi
 
-        echo "Do you want Godot Voxel? (y/n)"
-        read B_GD_VOXEL
+        if [ $B_COMMUNITY = "y" ]
+        then
+            # community engine modules
+            echo "Do you want LimboAI? (Y/n)"
+            read B_LIMBOAI
+            if [ "$B_LIMBOAI" == "" ]
+            then
+                B_LIMBOAI="y"
+            fi
+            if [ $B_LIMBOAI == 'y' ]
+            then
+                echo "Do you want the latest limboai nightly build? (Y/n)"
+                read B_LIMBOAI_NIGHTLY
+                if [ "$B_LIMBOAI_NIGHTLY" == "" ]
+                then
+                    B_LIMBOAI_NIGHTLY="y"
+                fi
+            fi
+
+            echo "Do you want Godot Steam? (y/N) NOTE: requires Steamworks SDK to be unzipped in an adjacent folder called steam_sdk, and put your steam_appid.txt in there, too!"
+            echo "Double NOTE: Don't cross-compile on Linux for Windows using MinGW with this module; compile natively on Windows using MSVC instead. See GodotSteam Github note."
+            read B_GD_STEAM
+            if [ "$B_GD_STEAM" == "" ]
+            then
+                B_GD_STEAM="n"
+            fi
+
+            echo "Do you want Godot Voxel? (y/N)"
+            read B_GD_VOXEL
+            if [ "$B_GD_VOXEL" == "" ]
+            then
+                B_GD_VOXEL="n"
+            fi
+        fi
+
     else # if only rebuilding/creating templates, simply rename existing godot folder.
         echo "Welcome to the Cult of the Blue Robot."
-        echo "Choose your Godot version (e.g. 4.1.2-stable, 4.1, master)"
+        echo "Choose your Godot version (e.g. 4.1.3-stable, 4.1, master) or default - latest stable release."
         read GODOT_VERSION
+        if [ "$GODOT_VERSION" == "" ]
+        then
+            GODOT_VERSION="4.1.3-stable"
+        fi
         cd ..
         mv godot-* godot
     fi
 
-    if [ $BUILD_TYPE == "rebuild" ]
+    if [ $BUILD_TYPE == "rebuild" ] # just a warning, not a code header
     then
         echo "Below settings should match previous builds (I need to learn how to cache this)."
     fi
 
     # build options
-    echo "What platform are you building for? (linuxbsd, windows, macos, server, etc.)"
+    echo "What platform are you building for? (LINUXBSD, windows, macos, server, etc.)"
     read PLATFORM
+    if [ "$PLATFORM" == "" ]
+    then
+        PLATFORM="linuxbsd"
+    fi
     if [ $PLATFORM == "macos" ] # currently unsupported for cross-compile due to MoltenVK errors.
     then
         echo "NOTE: If osxcross initial setup is struggling, try installing the libbz2-dev package for SDK extraction."
         export OSXCROSS_ROOT="$HOME/osxcross"
     fi
-    echo "What architecture are you building on? (x86_64, arm64, rv64, wasm32, etc.)"
+    echo "What architecture are you building on? (X86_64, arm64, rv64, wasm32, etc.)"
     read ARCH
-    echo "How many threads would you like to use for building?"
+    if [ "$ARCH" == "" ]
+    then
+        ARCH="x86_64"
+    fi
+    echo "How many threads would you like to use for building? (default 16)"
     read THREADCOUNT
-    echo "Build export templates as well? (y/n)"
+    if [ "$THREADCOUNT" == "" ]
+    then
+        THREADCOUNT="16"
+    fi
+    echo "Build export templates as well? (y/N)"
     read TEMPLATES
+    if [ "$TEMPLATES" == "" ]
+    then
+        TEMPLATES="n"
+    fi
     if [ $TEMPLATES == "y" ]
     then
         echo "Choose template type (template_debug/template_release/BOTH)"
         read TEMPLATE_TYPE
-        echo "Do you want PCK encryption support? (y/n)"
+        if [ "$TEMPLATE_TYPE" == "" ]
+        then
+            TEMPLATE_TYPE="both"
+        fi
+        echo "Do you want PCK encryption support? (y/N)"
         read B_ENCRYPT
+         if [ "$B_ENCRYPT" == "" ]
+        then
+            B_ENCRYPT="n"
+        fi
         if [ $B_ENCRYPT == "y" ]
         then
             export SCRIPT_AES256_ENCRYPTION_KEY="8198992e4523238ab8f520d65b7c54b83347c33a6897855547e3639a594d6a91"
         fi
     fi
-    echo "Enable .NET (C#) support? (yes/no) NOTE: requires .NET SDK 6/7 to be installed, ~/.dotnet may also need to be in PATH."
+    echo "Enable .NET (C#) support? (YES/no) NOTE: requires .NET SDK 6/7 to be installed, ~/.dotnet may also need to be in PATH."
     read B_DOTNET
-    echo "Enable dev_mode for warnings as errors and unit testing? (yes/no) This is useful when doing engine development. NOTE: seems to not play well with some modules."
+    if [ "$B_DOTNET" == "" ]
+    then
+        B_DOTNET="yes"
+    fi
+    echo "Enable dev_mode for warnings as errors and unit testing? (yes/NO) This is useful when doing engine development. NOTE: seems to not play well with some modules."
     read B_DEVMODE
+    if [ "$B_DEVMODE" == "" ]
+    then
+        B_DEVMODE="no"
+    fi
     if [ $B_DEVMODE == "no" ]
     then
         B_DEVBUILD="no"
         B_DEBUGSYMBOLS="no"
-        echo "Enable production mode for better optimization and portability? (yes/no) This is useful for exporting final game builds."
+        echo "Enable production mode for better optimization and portability? (yes/NO) This is useful for exporting final game builds."
         read B_OPTIMIZE
+        if [ "$B_OPTIMIZE" == "" ]
+        then
+            B_OPTIMIZE="no"
+        fi
     else
         B_OPTIMIZE="no"
-        echo "Enable dev_build mode for disabling optimization and enabling debug symbols? (yes/no) Also useful for engine development. NOTE: seems to not play well with some modules."
+        echo "Enable dev_build mode for disabling optimization and enabling debug symbols? (yes/NO) Also useful for engine development. NOTE: seems to not play well with some modules."
         read B_DEVBUILD
+        if [ "$B_DEVBUILD" == "" ]
+        then
+            B_DEVBUILD="no"
+        fi
         if [ $B_DEVBUILD == "yes" ]
         then
             echo "Additionally enable debugging symbols for debugger and profiler support? (yes/no)"
             read B_DEBUGSYMBOLS
+            if [ "$B_DEBUGSYMBOLS" == "" ]
+            then
+                B_DEBUGSYMBOLS="yes"
+            fi
         else
             B_DEBUGSYMBOLS="no"
         fi
     fi
-    echo "Choose an optimization level. This should align well with previous choices. (speed_trace/speed/size/debug/none)"
+    echo "Choose an optimization level. This should align well with previous choices. (SPEED_TRACE/speed/size/debug/none)"
     read OPT_LEVEL
-    echo "Use single or double precision? (single/double)"
+    if [ "$OPT_LEVEL" == "" ]
+    then
+        OPT_LEVEL="speed_trace"
+    fi
+    echo "Use single or double precision? (SINGLE/double)"
     read PRECISION_LEVEL
+    if [ "$PRECISION_LEVEL" == "" ]
+    then
+        PRECISION_LEVEL="single"
+    fi
+
     if [ $B_OPTIMIZE == 'no' ]
     then
-        echo "Use Clang & LLVM instead of GCC to compile? (yes/no)"
+        echo "Use Clang & LLVM instead of GCC to compile? (YES/no)"
         read B_CLANG
-        echo "Use the Mold linker for faster compiles? (y/n) This may not work on all systems after setup."
+        if [ "$B_CLANG" == "" ]
+        then
+            B_CLANG="yes"
+        fi
+        echo "Use the Mold linker for faster compiles? (y/N) This may not work on all systems after setup."
         read B_MOLD
+        if [ "$B_MOLD" == "" ]
+        then
+            B_MOLD="no"
+        fi
     else
         B_CLANG="no"
         B_MOLD="no"
     fi
     if [ $PLATFORM == "linuxbsd" ]
     then
-        echo "Launch editor upon completion? (y/n)"
+        echo "Launch editor upon completion? (Y/n)"
         read B_EDITOR
+        if [ "$B_EDITOR" == "" ]
+        then
+            B_EDITOR="y"
+        fi
     else
         B_EDITOR="n"
     fi
@@ -242,7 +412,7 @@ else
         git clone https://github.com/godotengine/godot.git -b "$GODOT_VERSION"
 
         # clone modules, copy each into godot modules folder
-        if [ $B_GD_STEAM == 'y' ] 
+        if [ "$B_GD_STEAM" == 'y' ] 
         then
             git clone https://github.com/CoaguCo-Industries/GodotSteam.git -b v4.4.1
             mv GodotSteam/ godotsteam/
@@ -253,7 +423,7 @@ else
             cp -rv steam_sdk/sdk/redistributable_bin/ godot/modules/godotsteam/sdk/
         fi
 
-        if [ $B_LIMBOAI == 'y' ]
+        if [ "$B_LIMBOAI" == 'y' ]
         then
             if [ $B_LIMBOAI_NIGHTLY == 'y' ]
             then
@@ -264,7 +434,7 @@ else
             cp -rv limboai/ godot/modules/
         fi
 
-        if [ $B_GD_VOXEL == 'y' ]
+        if [ "$B_GD_VOXEL" == 'y' ]
         then
             git clone https://github.com/Zylann/godot_voxel.git -b master
             mv godot_voxel/ voxel/
@@ -275,7 +445,7 @@ else
     cd godot/
 
     # rename Godot folder appropriately (do this late to avoid errors)
-    if [ $B_GD_STEAM == 'y' ] || [ $B_LIMBOAI == 'y' ] || [ $B_GD_VOXEL == 'y' ]
+    if [ "$B_GD_STEAM" == 'y' ] || [ "$B_LIMBOAI" == 'y' ] || [ "$B_GD_VOXEL" == 'y' ]
     then
         mv ../godot ../godot-"$GODOT_VERSION"-custom
     else
@@ -374,7 +544,7 @@ else
     if [ $TEMPLATES == 'y' ]
     then
         echo "#### Now building export template(s). ####"
-        if [ $TEMPLATE_TYPE == 'BOTH' ]
+        if [ $TEMPLATE_TYPE == 'both' ]
         then
             pyston-scons platform="$PLATFORM" arch="$ARCH" target=template_debug dev_mode="$B_DEVMODE" dev_build="$B_DEVBUILD" debug_symbols="$B_DEBUGSYMBOLS" production="$B_OPTIMIZE" optimize="$OPT_LEVEL" module_mono_enabled="$B_DOTNET" use_llvm="$B_CLANG" precision="$PRECISION_LEVEL" -j"$THREADCOUNT"
             pyston-scons platform="$PLATFORM" arch="$ARCH" target=template_release dev_mode="$B_DEVMODE" dev_build="$B_DEVBUILD" debug_symbols="$B_DEBUGSYMBOLS" production="$B_OPTIMIZE" optimize="$OPT_LEVEL" module_mono_enabled="$B_DOTNET" use_llvm="$B_CLANG" precision="$PRECISION_LEVEL" -j"$THREADCOUNT"
